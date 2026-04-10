6535a072df7ab789230125603c010e42f16c7225
AbdulQayyumAhmed <aq2541612@gmail.com>
2026-04-10 13:17:33 +0500

    backend changes

--- backend_folder/app/config.py
+++ backend_folder/app/config.py
@@ -2,16 +2,14 @@ from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 
-# Load .env file
 load_dotenv()
 
-MONGO_URL = os.getenv("MONGO_URI", "mongodb+srv://user:uS3er2060@bootcamptracker.roknckd.mongodb.net/")
-DB_NAME = os.getenv("DB_NAME", "test")
-
+MONGO_URL = os.getenv("MONGO_URI")
 client = MongoClient(MONGO_URL)
-db = client[DB_NAME]
+
+db = client[os.getenv("DB_NAME", "test")]
 
 users_col = db["users"]
 assignments_col = db["assignments"]
 submitted_col = db["student_assignments"]
-notifications_col = db["notifications"]
+notifications_col = db["notifications"]
